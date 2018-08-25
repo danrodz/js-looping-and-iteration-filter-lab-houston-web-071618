201c98516b722735ca3f54121b3ed18fe2eb6bf5
danrodz <daniel.a.rodriguez@dr.com>
2018-08-25 18:29:24 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Code your solution in this file
+const findMatching = (list, name) =>
+  list.filter((driverName) => driverName.toLowerCase() === name.toLowerCase());
+
+const fuzzyMatch = (list, partialName) => {
+  let lengthOfName = partialName.length;
+  return list.filter(
+    (driverName) => driverName.slice(0, lengthOfName) === partialName
+  );
+};
+
+const matchName = (list, name) =>
+  list.filter((driver) => driver.name.toLowerCase() === name.toLowerCase());
